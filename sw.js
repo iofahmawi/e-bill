f813374e155d71e569201d40c5b05a8ad088f0b4
iofahmawi <mo.fahmawi@gmail.com>
2025-10-31 09:00:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,45 @@
-const CACHE_NAME = 'e-bill-calculator-v3'; // غيرنا الرقم لفرض التحديث
+const CACHE_NAME = 'e-bill-calculator-cache-v1';
 const urlsToCache = [
-    '/',
-    'index.html',
-    'manifest.json',
-    'icon-1024.png'
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-1024.png',
+  'icon.png',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .catch(err => {
+            console.error('Failed to cache one or more resources during install:', err);
+          });
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
