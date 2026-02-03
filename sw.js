5a5869e4f15a5b67d805bf74f59c5bae659661ea
iofahmawi <mo.fahmawi@gmail.com>
2026-02-03 09:37:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 
 // 1. تغيير اسم الكاش وتحديث الإصدار
-const CACHE_NAME = 'e-bill-dynamic-v17';
+const CACHE_NAME = 'e-bill-dynamic-v18';
 
 // 2. نخزن فقط الملفات الأساسية جداً لضمان نجاح التثبيت
 const urlsToCache = [
