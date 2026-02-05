2c3daafc4bf448e289e3e9eb30a367bfe79036d1
iofahmawi <mo.fahmawi@gmail.com>
2026-02-05 12:38:30 +0300

    Change cache name to 'e-bill-dynamic-v35'
    Updated cache name to version 35 for service worker.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 
 // 1. تغيير اسم الكاش وتحديث الإصدار
-const CACHE_NAME = 'e-bill-dynamic-v34';
+const CACHE_NAME = 'e-bill-dynamic-v35';
 
 // 2. نخزن فقط الملفات الأساسية جداً لضمان نجاح التثبيت
 const urlsToCache = [
